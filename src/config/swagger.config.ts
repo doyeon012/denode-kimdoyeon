b4674e699d19d28396a32746ea8d4f5e9812ee19
doyeon kim <kim2004on@gmail.com>
2025-06-16 00:20:27 +0900

    feat: swagger setup

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,32 @@
+import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
+import * as path from 'path';
+import AuthGuard from 'src/middleware/auth/auth.guard';
+
+
+export function setUpSwagger(app: INestApplication) {
+  const configService: ConfigService = app.get(ConfigService);
+  const config  = new DocumentBuilder()
+    .setTitle('denode-kimdoyeon API')
+    .setDescription('denode-kimdoyeon API documentation')
+    .setVersion(configService.get('version')!)
+    .addBearerAuth(
+      {
+        type: 'apiKey',
+        scheme: 'bearer',
+        name: AuthGuard.ACCESS_TOKEN_HEADER,
+        in: 'header',
+        description: 'Enter your access token',
+      },
+      AuthGuard.ACCESS_TOKEN_HEADER,
+    )
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
+  const specJsonPath = path.join(configService.get('PWD') || process.cwd(), './swagger-spec.json');
+  writeFileSync(specJsonPath, JSON.stringify(documentFactory));
+}
