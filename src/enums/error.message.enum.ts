332f781a425b3f3b01b6117b591ec93306bc62d7
doyeon kim <kim2004on@gmail.com>
2025-06-16 00:26:15 +0900

    feat: error.message.enum add

--- src/enums/error.message.enum.ts
+++ src/enums/error.message.enum.ts
@@ -1,2 +1,6 @@
-export const ErrorMessageType = {} as const;
+export const ErrorMessageType = {
+  PASSWORD_MISMATCH: '비밀번호가 일치하지 않습니다.',
+  USER_NOT_FOUND: '사용자를 찾을 수 없습니다.',
+  INVALID_PASSWORD: '잘못된 비밀번호입니다.',
+} as const;
 export type ErrorMessageEnumType = (typeof ErrorMessageType)[keyof typeof ErrorMessageType];
