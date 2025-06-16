f6c4d2b5d3bd0d1ab770599891a2c2b216487309
doyeon kim <kim2004on@gmail.com>
2025-06-16 20:45:17 +0900

    feat: inboundInventory, outboundInventory controller, service add

--- src/domain/product/controller/product.controller.ts
+++ src/domain/product/controller/product.controller.ts
@@ -1,17 +1,28 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiBearerAuth, ApiBody, ApiConsumes, ApiOkResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { Body, Controller, Delete, Post, Query } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiOkResponse,
+  ApiQuery,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { ProductService } from '../service/product.service';
 import { ProductCreateRequest } from '../dto/request/product.create.request';
 import AuthGuard from 'src/middleware/auth/auth.guard';
 import { ErrorMessageType } from 'src/enums/error.message.enum';
 import { AuthUser, Token } from 'src/decorator/toekn.decorator';
+import { InventoryInboundRequest } from '../dto/request/inventory.inbound.request';
+import { InventoryOutboundRequest } from '../dto/request/inventory.outbound.request';
 
 @ApiTags('product')
 @Controller('product')
 export class ProductController {
   constructor(private productService: ProductService) {}
 
-  @Post('create')
+  @Post('product/create')
   @ApiBearerAuth(AuthGuard.ACCESS_TOKEN_HEADER)
   @ApiOkResponse({ description: 'Product created successfully' })
   @ApiUnauthorizedResponse({ description: ErrorMessageType.UNAUTHORIZED })
@@ -22,5 +33,33 @@ export class ProductController {
   })
   async createProduct(@Body() request: ProductCreateRequest, @Token() user: AuthUser): Promise<number> {
     return this.productService.createProduct(request, user.id);
+  }
+
+  @Post('inventory/inbound')
+  @ApiBearerAuth(AuthGuard.ACCESS_TOKEN_HEADER)
+  @ApiOkResponse({ description: 'Inventory inbound successfully' })
+  @ApiUnauthorizedResponse({ description: ErrorMessageType.UNAUTHORIZED })
+  @ApiBadRequestResponse({ description: ErrorMessageType.NOT_FOUND_INVENTORY })
+  @ApiConsumes('application/json')
+  @ApiBody({
+    type: InventoryInboundRequest,
+    description: 'Inventory inbound information',
+  })
+  async createInventory(@Body() request: InventoryInboundRequest, @Token() user: AuthUser): Promise<void> {
+    await this.productService.inboundInventory(request, user.id);
+  }
+
+  @Post('inventory/outbound')
+  @ApiBearerAuth(AuthGuard.ACCESS_TOKEN_HEADER)
+  @ApiOkResponse({ description: 'Inventory outbound successfully' })
+  @ApiUnauthorizedResponse({ description: ErrorMessageType.UNAUTHORIZED })
+  @ApiBadRequestResponse({ description: ErrorMessageType.NOT_FOUND_INVENTORY })
+  @ApiConsumes('application/json')
+  @ApiBody({
+    type: InventoryOutboundRequest,
+    description: 'Inventory outbound information',
+  })
+  async deleteInventory(@Body() request: InventoryOutboundRequest, @Token() user: AuthUser): Promise<void> {
+    await this.productService.outboundInventory(request, user.id);
   }
 }
--- src/domain/product/service/product.service.ts
+++ src/domain/product/service/product.service.ts
@@ -1,24 +1,95 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ProductComponent } from '../component/product.component';
 import { InventoryComponent } from '../component/inventory.component';
 import { ProductCreateRequest } from '../dto/request/product.create.request';
 import { CreateProductDto } from '../dto/request/create.product.dto';
+import { InventoryInboundRequest } from '../dto/request/inventory.inbound.request';
+import { ErrorMessageType } from 'src/enums/error.message.enum';
+import { InventoryOutboundRequest } from '../dto/request/inventory.outbound.request';
+import { StockHistoryComponent } from '../component/stock.history.component';
+import { Transactional } from 'typeorm-transactional';
 
 @Injectable()
 export class ProductService {
   constructor(
     private productComponent: ProductComponent,
     private inventoryComponent: InventoryComponent,
-    private stockHistoryComponent: InventoryComponent,
+    private stockHistoryComponent: StockHistoryComponent,
   ) {}
 
   public async createProduct(request: ProductCreateRequest, requesterId: number): Promise<number> {
-    
+    const product = await this.productComponent.findByName(request.name);
+    if (product) {
+      throw new BadRequestException(ErrorMessageType.PRODUCT_ALREADY_EXISTS);
+    }
+
     const createProductDto: CreateProductDto = {
       ...request,
-      userId: requesterId
+      userId: requesterId,
+    };
+
+    return await this.productComponent.create(createProductDto);
+  }
+
+  @Transactional()
+  public async inboundInventory(request: InventoryInboundRequest, requesterId: number): Promise<void> {
+    const dateOnly = request.expiryDate.toISOString().split('T')[0];
+    const existingInventory = await this.inventoryComponent.findByProductAndDate(request.productId, dateOnly);
+
+    let inventoryId: number;
+    if (existingInventory) {
+      await this.inventoryComponent.updateQuantity(existingInventory.id, existingInventory.quantity + request.quantity);
+      inventoryId = existingInventory.id;
+    } else {
+      inventoryId = await this.inventoryComponent.create({
+        ...request,
+        userId: requesterId,
+      });
     }
-    
-    return this.productComponent.create(createProductDto);
+
+    await this.stockHistoryComponent.create({
+      type: 'IN',
+      inventoryId: inventoryId,
+      quantity: request.quantity,
+      reason: request.reason,
+      productId: request.productId,
+    });
+  }
+
+  @Transactional()
+  public async outboundInventory(request: InventoryOutboundRequest, requesterId: number): Promise<void> {
+    const inventories = await this.inventoryComponent.findByProductIdOrderByExpiryDate(request.productId);
+
+    let remainingQuantity = request.quantity;
+
+    for (const inventory of inventories) {
+      if (remainingQuantity <= 0) {
+        break;
+      }
+
+      if (inventory.quantity <= remainingQuantity) {
+        await this.stockHistoryComponent.create({
+          type: 'OUT',
+          inventoryId: inventory.id,
+          quantity: inventory.quantity,
+          reason: request.reason,
+          productId: request.productId,
+        });
+
+        remainingQuantity -= inventory.quantity;
+        await this.inventoryComponent.delete(inventory.id);
+      } else {
+        await this.stockHistoryComponent.create({
+          type: 'OUT',
+          inventoryId: inventory.id,
+          quantity: remainingQuantity,
+          reason: request.reason,
+          productId: request.productId,
+        });
+
+        await this.inventoryComponent.updateQuantity(inventory.id, inventory.quantity - remainingQuantity);
+        remainingQuantity = 0;
+      }
+    }
   }
 }
